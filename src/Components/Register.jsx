c702f1f3a629258a5b3402d638149723cac0b3ce
balram <85399069+balram763@users.noreply.github.com>
2025-03-08 14:52:39 +0530

    Update Register.jsx

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,6 +1,20 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import ShoppingContext from '../providers/ShoppingContext'
 
 const Register = () => {
+    
+        const {handleRegister,user} = useContext(ShoppingContext)
+        const Navigate = useNavigate()
+
+
+    useEffect(()=>{
+        if(user !== null && localStorage.getItem("userdetail") !== null){
+        Navigate('/')      
+        }
+    },[user])
+
+    
     const [formData,setFormData] = useState({
         name : '',
         email : '',
@@ -15,7 +29,13 @@ const Register = () => {
     }
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(formData)
+        if(formData.password !== formData.password2){
+            window.alert('Re-Enter Confirm Password')
+        }
+        if(formData.password == formData.password2){
+            handleRegister(formData)
+
+        }
     }
   return (
     <>
@@ -25,8 +45,8 @@ const Register = () => {
         <form onSubmit={(e)=>handleSubmit(e)}>
             <input name='name' value={formData.name} onChange={handleChange} style={{height:'50px',marginBlock:'20px'}} type="text" className="form-control" placeholder='Enter Name'/>
             <input  name='email' value={formData.email} onChange={handleChange} style={{height:'50px',marginBlock:'20px'}} type="email" className="form-control" placeholder='Enter Email'/>
-            <input minLength={4} maxLength={15} name='password' value={formData.password} onChange={handleChange} style={{height:'50px',marginBlock:'20px'}} type="password" className="form-control" placeholder='Enter Password'/>
-            <input minLength={4} maxLength={15} name='password2' value={formData.password2} onChange={handleChange} style={{height:'50px',marginBlock:'20px'}} type="password" className="form-control" placeholder='Enter Password'/>
+            <input minLength={8} maxLength={15} name='password' value={formData.password} onChange={handleChange} style={{height:'50px',marginBlock:'20px'}} type="password" className="form-control" placeholder='Enter Password'/>
+            <input minLength={8} maxLength={15} name='password2' value={formData.password2} onChange={handleChange} style={{height:'50px',marginBlock:'20px'}} type="password" className="form-control" placeholder='Enter Password'/>
             <button className="mt-2 btn form-control btn-primary">Register</button>
         </form>
     </div>
