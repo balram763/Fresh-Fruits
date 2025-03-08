30acf945c229ce706180c6400d05747745262141
balram <85399069+balram763@users.noreply.github.com>
2025-03-08 21:14:15 +0530

    Update AddedProduct.jsx

--- src/Components/AddedProduct.jsx
+++ src/Components/AddedProduct.jsx
@@ -1,24 +1,3 @@
-// import React, { useContext } from 'react'
-// import ShoppingContext from '../providers/ShoppingContext'
-// import Card from './Card'
-
-// const AddedProduct = () => {
-//     const {newProducts} = useContext(ShoppingContext)
-//     return (
-//         <>
-//     {
-//       newProducts?.map(user => {
-//         return ( <>
-//           <Card user={user} key={user.id}/></>
-//         )
-//       })
-//     }
-//         </>
-//       )
-// }
-
-// export default AddedProduct
-
 import React, { useContext } from 'react'
 import ShoppingContext from '../providers/ShoppingContext'
 import Card from './Card'
