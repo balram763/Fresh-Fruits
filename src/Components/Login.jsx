dcbf360ab9f0578a78a78f1d290187be68547bdd
balram <85399069+balram763@users.noreply.github.com>
2025-03-18 21:52:49 +0530

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -84,7 +84,7 @@ const Login = () => {
              Log In
           </button>
         </form>
-        <Link to="/login" className="text-primary mt-4 fw-bold">
+        <Link to="/register" className="text-primary mt-4 fw-bold">
                 Already have account? LogIn
               </Link>
     </div>
